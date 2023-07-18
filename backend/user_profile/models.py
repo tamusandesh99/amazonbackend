a5bc3076753f5c3968058f14dc097b96a4c3b23b
tamusandesh99 <tamusandesh99@gmail.com>
2023-07-17 22:00:41 -0400

    changed model

--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -7,7 +7,6 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     website_link = models.CharField(max_length=255, default='')
-    city = models.CharField(max_length=20, default='')
 
     def __str__(self):
         return self.website_link
