61577ecc4264b8e7d66cae87a8f26db0da541aa0
tamusandesh99 <tamusandesh99@gmail.com>
2023-12-06 20:48:38 -0500

    fix: post model for likes and comments

--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -1,5 +1,6 @@
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
+from django.utils import timezone
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -13,6 +14,9 @@ class Post(models.Model):
     description = models.CharField(max_length=255)
     images = ArrayField(models.TextField(), blank=True, default=list, null=True)
     links = ArrayField(models.URLField(), default=list, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)  # Initialize likes as 0
+    comments = ArrayField(models.CharField(max_length=255), default=list, blank=True)
 
     def __str__(self):
         return self.title
