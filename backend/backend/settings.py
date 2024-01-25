5361ca77ad7ac099d159add1d9ba8c8842576f53
tamusandesh99 <tamusandesh99@gmail.com>
2024-01-24 19:07:46 -0500

    add: requirements file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://0.0.0.0',
     'http://127.0.0.1',
-    'https://127.0.0.1'
+    'https://127.0.0.1',
 ]
 CSRF_TRUSTED_ORIGINS = ['http://localhost:3000',
                         'https://localhost:3000',
@@ -73,8 +73,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
